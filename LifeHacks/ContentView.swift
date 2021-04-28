7d5159df0ce5c08859952f5d5a0e3b8f38e9c1dd
Ethan Smith <edhsmith@icloud.com>
2021-04-28 16:51:11 +1200

    Initial Commit

---
+++ LifeHacks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LifeHacks
+//
+//  Created by Ethan Smith on 28/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
