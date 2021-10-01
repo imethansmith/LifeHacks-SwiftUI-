07cdac9163e8a3ec703fbf1723eb0cf5394b9dd4
Ethan Smith <edhsmith@icloud.com>
2021-10-01 20:47:07 +1300

    Added state persistence with SceneController in Content and TopQuestionsView.

--- LifeHacks/ContentView.swift
+++ LifeHacks/ContentView.swift
@@ -9,41 +9,43 @@ import SwiftUI
 
 //MARK: - ContentView
 struct ContentView: View {
+    @State private var isLoggingIn = false
     @EnvironmentObject private var stateController: StateController
     @AppStorage(LifehacksApp.Keys.isLoggedIn) private var isLoggedIn = false
+    @SceneStorage("ContentView.SelectedTab") private var selectedTab = 0
     
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             NavigationView {
                 TopQuestionsView()
             }
             .tabItem { Label("Top Questions", systemImage: "list.number") }
-            
+            .tag(0)
             NavigationView {
                 TopTagsView()
             }
             .tabItem { Label("Tags", systemImage: "tag") }
-            
+            .tag(1)
             NavigationView {
                 TopUsersView()
             }
             .tabItem { Label("Users", systemImage: "person.2") }
-            
+            .tag(2)
             NavigationView {
                 ProfileView(user: stateController.mainUser, isMainUser: true)
             }
             .tabItem { Label("Profile", systemImage: "person.circle") }
-            
+            .tag(3)
             NavigationView {
                 SettingsView()
             }
             .tabItem { Label("Settings", systemImage: "gear") }
-            
+            .tag(4)
         }
         .accentColor(stateController.theme.accentColor)
         .environment(\.theme, stateController.theme)
-        .onAppear { isLoggedIn = !isLoggedIn}
-        .fullScreenCover(isPresented: $isLoggedIn) {
+        .onAppear { isLoggingIn = !isLoggedIn}
+        .fullScreenCover(isPresented: $isLoggingIn) {
             LoginView()
                 .accentColor(stateController.theme.accentColor)
         }
